cf886e2ed6731d3126747b728f43f1e4a004a042
Matt Johnson <mattjohnson@cvg-c02x485bjg5j.lan>
2020-04-29 16:27:29 -0400

    Made changes to game and player tables to allow sorting on each column.

--- app/views/GameHistory/index.js
+++ app/views/GameHistory/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {View, ScrollView} from 'react-native';
 import {withTheme, Text, DataTable} from 'react-native-paper';
@@ -6,42 +6,137 @@ import Header from '../Header';
 
 const GameHistory = props => {
   const colors = props.theme.colors;
-  const gameDataTable = props.games.map((game, index) => {
-    return (
-      <DataTable.Row key={index}>
-        <DataTable.Cell>{game.date}</DataTable.Cell>
-        <DataTable.Cell numeric>{game.players.length}</DataTable.Cell>
-        <DataTable.Cell numeric>{game.totalThrows}</DataTable.Cell>
-        {game.active ? (
-          <DataTable.Cell
-            onPress={() =>
-              props.navigation.navigate('Play', {
-                game: index,
-              })
-            }
-            numeric>
-            <Text style={{color: colors.primary}}>Resume</Text>
-          </DataTable.Cell>
-        ) : (
-          <DataTable.Cell numeric>{game.winner}</DataTable.Cell>
-        )}
-      </DataTable.Row>
-    );
-  });
+
+  const [gameList, setGameList] = useState(undefined);
+
+  useEffect(() => {
+    if (props.players !== undefined) {
+      setGameList(props.games);
+    }
+  }, [props.games]);
+
+  let gameDataTable = <></>;
+
+  if (gameList !== undefined) {
+    gameDataTable = gameList.map((game, index) => {
+      return (
+        <DataTable.Row key={index}>
+          <DataTable.Cell>{game.date}</DataTable.Cell>
+          <DataTable.Cell numeric>{game.players.length}</DataTable.Cell>
+          <DataTable.Cell numeric>{game.totalThrows}</DataTable.Cell>
+          {game.active ? (
+            <DataTable.Cell
+              onPress={() =>
+                props.navigation.navigate('Play', {
+                  game: index,
+                })
+              }
+              numeric>
+              <Text style={{color: colors.primary}}>Resume</Text>
+            </DataTable.Cell>
+          ) : (
+            <DataTable.Cell numeric>{game.winner}</DataTable.Cell>
+          )}
+        </DataTable.Row>
+      );
+    });
+  }
+
+  const [sortOrder, setSortOrder] = useState('descending');
+
+  const sortArray = column => {
+    let order = sortOrder === 'descending' ? -1 : 1;
+
+    let orderedArray = gameList;
+
+    setSortOrder(sortOrder === 'descending' ? 'ascending' : 'descending');
+
+    switch (column) {
+      case 'dates':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.date > b.date) {
+            comparison = 1;
+          } else if (a.date < b.date) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setGameList(orderedArray);
+        break;
+      case 'players':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.players.length > b.players.length) {
+            comparison = 1;
+          } else if (a.players.length < b.players.length) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setGameList(orderedArray);
+        break;
+      case 'throws':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.totalThrows > b.totalThrows) {
+            comparison = 1;
+          } else if (a.totalThrows < b.totalThrows) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setGameList(orderedArray);
+        break;
+      case 'winner':
+        orderedArray = orderedArray.sort(function(a, b) {
+          const playerA = a.winner.toUpperCase();
+          const playerB = b.winner.toUpperCase();
+
+          let comparison = 0;
+          if (playerA > playerB) {
+            comparison = 1;
+          } else if (playerA < playerB) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setGameList(orderedArray);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <>
       <Header title="Game History" />
       <View style={{flex: 1, padding: 16, backgroundColor: colors.background}}>
-        <DataTable>
-          <DataTable.Header>
-            <DataTable.Title>Date</DataTable.Title>
-            <DataTable.Title numeric>Players</DataTable.Title>
-            <DataTable.Title numeric>Throws</DataTable.Title>
-            <DataTable.Title numeric>Winner</DataTable.Title>
-          </DataTable.Header>
-          <ScrollView>{gameDataTable}</ScrollView>
-        </DataTable>
+        {props.games !== undefined ? (
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title onPress={() => sortArray('dates')}>
+                Date
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('players')}>
+                Players
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('throws')}>
+                Throws
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('winner')}>
+                Winner
+              </DataTable.Title>
+            </DataTable.Header>
+            <ScrollView>{gameDataTable}</ScrollView>
+          </DataTable>
+        ) : (
+          <></>
+        )}
       </View>
     </>
   );
--- app/views/PlayerHistory/index.js
+++ app/views/PlayerHistory/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import {View, ScrollView} from 'react-native';
 import {withTheme, DataTable} from 'react-native-paper';
@@ -7,34 +7,158 @@ import Header from '../Header';
 const PlayerHistory = props => {
   const colors = props.theme.colors;
 
-  const playerTableData = props.players.map((player, index) => {
-    return (
-      <DataTable.Row key={index}>
-        <DataTable.Cell>{player.name}</DataTable.Cell>
-        <DataTable.Cell numeric>{player.totalGames}</DataTable.Cell>
-        <DataTable.Cell numeric>{player.totalWins}</DataTable.Cell>
-        <DataTable.Cell numeric>{player.totalThrows}</DataTable.Cell>
-        <DataTable.Cell numeric>{player.totalMisses}</DataTable.Cell>
-        <DataTable.Cell numeric>{player.totalPoints}</DataTable.Cell>
-      </DataTable.Row>
-    );
-  });
+  const [playerList, setPlayerList] = useState(undefined);
+
+  useEffect(() => {
+    if (props.players !== undefined) {
+      setPlayerList(props.players);
+    }
+  }, [props.players]);
+
+  let playerTableData = <></>;
+
+  if (playerList !== undefined) {
+    playerTableData = playerList.map((player, index) => {
+      return (
+        <DataTable.Row key={index}>
+          <DataTable.Cell>{player.name}</DataTable.Cell>
+          <DataTable.Cell numeric>{player.totalGames}</DataTable.Cell>
+          <DataTable.Cell numeric>{player.totalWins}</DataTable.Cell>
+          <DataTable.Cell numeric>{player.totalThrows}</DataTable.Cell>
+          <DataTable.Cell numeric>{player.totalMisses}</DataTable.Cell>
+          <DataTable.Cell numeric>{player.totalPoints}</DataTable.Cell>
+        </DataTable.Row>
+      );
+    });
+  }
+
+  const [sortOrder, setSortOrder] = useState('descending');
+
+  const sortArray = column => {
+    let order = sortOrder === 'descending' ? -1 : 1;
+
+    let orderedArray = playerList;
+
+    setSortOrder(sortOrder === 'descending' ? 'ascending' : 'descending');
+
+    switch (column) {
+      case 'players':
+        orderedArray = orderedArray.sort(function(a, b) {
+          const playerA = a.name.toUpperCase();
+          const playerB = b.name.toUpperCase();
+
+          let comparison = 0;
+          if (playerA > playerB) {
+            comparison = 1;
+          } else if (playerA < playerB) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setPlayerList(orderedArray);
+        break;
+      case 'games':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.totalGames > b.totalGames) {
+            comparison = 1;
+          } else if (a.totalGames < b.totalGames) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setPlayerList(orderedArray);
+        break;
+      case 'wins':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.totalWins > b.totalWins) {
+            comparison = 1;
+          } else if (a.totalWins < b.totalWins) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setPlayerList(orderedArray);
+        break;
+      case 'throws':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.totalThrows > b.totalThrows) {
+            comparison = 1;
+          } else if (a.totalThrows < b.totalThrows) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setPlayerList(orderedArray);
+        break;
+      case 'misses':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.totalMisses > b.totalMisses) {
+            comparison = 1;
+          } else if (a.totalMisses < b.totalMisses) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setPlayerList(orderedArray);
+        break;
+      case 'points':
+        orderedArray = orderedArray.sort(function(a, b) {
+          let comparison = 0;
+          if (a.totalPoints > b.totalPoints) {
+            comparison = 1;
+          } else if (a.totalPoints < b.totalPoints) {
+            comparison = -1;
+          }
+          return comparison * order;
+        });
+
+        setPlayerList(orderedArray);
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <>
       <Header title="Player History" />
       <View style={{flex: 1, padding: 16, backgroundColor: colors.background}}>
-        <DataTable>
-          <DataTable.Header>
-            <DataTable.Title>Player</DataTable.Title>
-            <DataTable.Title numeric>Games</DataTable.Title>
-            <DataTable.Title numeric>Wins</DataTable.Title>
-            <DataTable.Title numeric>Throws</DataTable.Title>
-            <DataTable.Title numeric>Misses</DataTable.Title>
-            <DataTable.Title numeric>Points</DataTable.Title>
-          </DataTable.Header>
-          <ScrollView>{playerTableData}</ScrollView>
-        </DataTable>
+        {props.players !== undefined ? (
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title onPress={() => sortArray('players')}>
+                Player
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('games')}>
+                Games
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('wins')}>
+                Wins
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('throws')}>
+                Throws
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('misses')}>
+                Misses
+              </DataTable.Title>
+              <DataTable.Title numeric onPress={() => sortArray('points')}>
+                Points
+              </DataTable.Title>
+            </DataTable.Header>
+            <ScrollView>{playerTableData}</ScrollView>
+          </DataTable>
+        ) : (
+          <></>
+        )}
       </View>
     </>
   );
@@ -44,4 +168,7 @@ const mapStateToProps = state => ({
   players: state.data.players,
 });
 
-export default connect(mapStateToProps, null)(withTheme(PlayerHistory));
+export default connect(
+  mapStateToProps,
+  null,
+)(withTheme(PlayerHistory));
